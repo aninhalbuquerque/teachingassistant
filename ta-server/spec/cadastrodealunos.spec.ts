c3b87084c4478200b4fc1c504f55d21e1da9464c
Aninha <analeticia1101@gmail.com>
2022-04-18 17:17:30 -0300

    adição de teste de atualizar aluno

--- ta-server/spec/cadastrodealunos.spec.ts
+++ ta-server/spec/cadastrodealunos.spec.ts
@@ -4,6 +4,29 @@ import { Aluno } from '../../common/aluno';
 describe("O cadastro de alunos", () => {
   var cadastro: CadastroDeAlunos;
 
+  function cadastrarAluno(nome:string, cpf:string) {
+    var aluno: Aluno = new Aluno();
+    aluno.nome = nome;
+    aluno.cpf = cpf;
+    cadastro.cadastrar(aluno);
+  }
+
+  function atualizaAluno(nome?:string, cpf?:string, email?:string, metas?:Map<string,string>) {
+    var aluno: Aluno = new Aluno();
+    if (nome) aluno.nome = nome;
+    if (cpf) aluno.cpf = cpf;
+    if (email) aluno.email = email;
+    if (metas) aluno.metas = metas;
+
+    cadastro.atualizar(aluno);
+  }
+
+  function expectSoUmAluno() {
+    expect(cadastro.getAlunos().length).toBe(1);
+    var aluno = cadastro.getAlunos()[0];
+    return aluno;
+  }
+
   beforeEach(() => cadastro = new CadastroDeAlunos())
 
   it("é inicialmente vazio", () => {
@@ -11,13 +34,9 @@ describe("O cadastro de alunos", () => {
   })
 
   it("cadastra alunos corretamente", () => {
-    var aluno: Aluno = new Aluno();
-    aluno.nome = "Mariana";
-    aluno.cpf = "683";
-    cadastro.cadastrar(aluno);
+    cadastrarAluno("Mariana","683");
 
-    expect(cadastro.getAlunos().length).toBe(1);
-    aluno = cadastro.getAlunos()[0];
+    var aluno = expectSoUmAluno();
     expect(aluno.nome).toBe("Mariana");
     expect(aluno.cpf).toBe("683");
     expect(aluno.email).toBe("");
@@ -25,17 +44,25 @@ describe("O cadastro de alunos", () => {
   })
 
   it("não aceita alunos com CPF duplicado", () => {
-    var aluno: Aluno = new Aluno();
-    aluno.nome = "Mariana";
-    aluno.cpf = "683";
-    cadastro.cadastrar(aluno);
+    cadastrarAluno("Mariana","683");
+    cadastrarAluno("Pedro","683");
 
-    aluno = new Aluno();
-    aluno.nome = "Pedro";
-    aluno.cpf = "683";
-    cadastro.cadastrar(aluno);
+    var aluno = expectSoUmAluno();
+    expect(aluno.nome).toBe("Mariana");
+  })
 
-    expect(cadastro.getAlunos().length).toBe(1);
+  it("cadastra e atualiza alunos corretamente", () => {
+    cadastrarAluno("Mariana","683");
+
+    atualizaAluno("Ana", "683", "alas3");
+
+    var aluno = expectSoUmAluno();
+    expect(aluno.nome).toBe("Ana");
+    expect(aluno.cpf).toBe("683");
+    expect(aluno.email).toBe("alas3");
+    expect(aluno.metas.size).toBe(0);
   })
 
 })
+
+
